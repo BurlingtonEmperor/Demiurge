e5c9f1d49538ab84114bdcbd5c80f49a4e85460a
Alexander Chang <alexanderchang208@gmail.com>
2026-04-23 20:49:13 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,1 @@
+#include "make_cpp_easier.h"
---
+++ src/make_cpp_easier.h
@@ -0,0 +1,31 @@
+#ifndef MAKE_CPP_EASIER_H
+#define MAKE_CPP_EASIER_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ranges>
+#include <numeric>
+
+void replaceAll(std::string& str, const std::string& from, const std::string& to);
+void console_log(std::string& str);
+void shiftFloatArray(float (&array_to_shift)[20], float new_value); 
+
+bool checkIncludes(std::string& text, const std::string& substring);
+
+int convertBoolToInt(bool& bool_value);
+int findLowestInArray(float (&array_to_check)[20]);
+
+float absoluteValue(float value_to_check);
+
+std::vector<std::string> splitByString(const std::string& str, const std::string& delimiter);
+std::vector<std::string> splitBySpaces(const std::string& str);
+
+std::string joinVectorItems_string(std::vector<std::string>& str_vector);
+std::string toUpperCase(std::string& str);
+std::string get_file_contents(const std::string& filename);
+
+#endif
