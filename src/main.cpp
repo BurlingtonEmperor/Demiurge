45ee4f42b75c552084b4a823b28715f45996af27
Alexander Chang <alexanderchang208@gmail.com>
2026-05-05 15:20:29 -0500

    add enump string map

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,27 @@
 #include <vector>
 #include <algorithm>
 #include <ranges>
+#include <unordered_map>
 
 #include "make_cpp_easier.h"
 #include "demiurge_math.h"
 
 enum class variable_types {
+  INT, // bytecode form: i
+  FLOAT, // f
+  LONG, // l
+  BOOL, // b
+  STRING, // s
+  INT_ARR, // ia
+  FLOAT_ARR, // fa
+  STRING_ARR, // sa
+  LONG_ARR, // la
+  BOOL_ARR // ba
+};
+
+enum class demiurge_commands_primary {
+  DISPLAY, // bytecode form: d
+  ACCEPT, // >
   INT, 
   FLOAT,
   LONG,
@@ -19,43 +35,83 @@ enum class variable_types {
   FLOAT_ARR,
   STRING_ARR,
   LONG_ARR,
-  BOOL_ARR
-};
-
-enum class demiurge_commands_primary {
-  DISPLAY,
-  ACCEPT,
-  INT,
-  FLOAT,
-  LONG,
-  BOOL,
-  STRING,
-  INT_ARR,
-  FLOAT_ARR,
-  STRING_ARR,
-  LONG_ARR,
   BOOL_ARR,
-  KEY_PROMPT,
-  ENCRYPT,
-  DECRYPT,
-  TEXT_OCR,
-  WAIT,
-  REPEAT,
-  STOP,
-  CLEAR,
-  EXECUTE,
+  KEY_PROMPT, // kp
+  ENCRYPT, // e
+  DECRYPT, // d
+  TEXT_OCR, // to
+  LLM, // large language model,
+  ILC, // iterative learning controller (sort of)
+  APM, // adaptive pattern matching
+  WAIT, // w
+  REPEAT, // r
+  STOP, // !
+  CLEAR, // _
+  EXECUTE, 
+  C, // <-- that's a comment indicator.
+  PLAY_SOUND,
   EDITOR_BG, // the editor commands start here.
   EDITOR_FONT,
   EDITOR_TEXT_COLOR,
   EDITOR_BGIMG,
   EDITOR_GLASS,
+  BLOCK_GLASS,
+  BLOCK_COLOR,
+  BLOCK_FONT,
+  BLOCK_TEXT_COLOR,
+  EDITOR_CSS, // editor commands end here.
+  DRAW_IMG, // canvas commands start here.
+  CLEAR_CANVAS,
+  WRITE_TEXT,
+  HTML_SOUND,
+  UNDEFINED
 };
 
 enum class limited_tools {
   RANDOM_INT,
   RANDOM_FLOAT,
-  ABSOLUTE_VALUE
+  ABSOLUTE_VALUE,
+  FIND_LOWEST_IN_ARRAY,
+  FIND_COMMON_IN_ARRAY
 };
+
+demiurge_commands_primary stringToEnum(const std::string& str) {
+  static const std::unordered_map<std::string, demiurge_commands_primary> commandMap = {
+    {"DISPLAY", demiurge_commands_primary::DISPLAY},
+    {"ACCEPT", demiurge_commands_primary::ACCEPT},
+    {"INT", demiurge_commands_primary::INT},
+    {"FLOAT", demiurge_commands_primary::FLOAT},
+    {"LONG", demiurge_commands_primary::LONG},
+    {"BOOL", demiurge_commands_primary::BOOL},
+    {"STRING", demiurge_commands_primary::STRING},
+    {"INT_ARR", demiurge_commands_primary::INT_ARR},
+    {"FLOAT_ARR", demiurge_commands_primary::FLOAT_ARR},
+    {"STRING_ARR", demiurge_commands_primary::STRING_ARR},
+    {"LONG_ARR", demiurge_commands_primary::LONG_ARR},
+    {"BOOL_ARR", demiurge_commands_primary::BOOL_ARR},
+    {"KEY_PROMPT", demiurge_commands_primary::KEY_PROMPT},
+    {"ENCRYPT", demiurge_commands_primary::ENCRYPT},
+    {"DECRYPT", demiurge_commands_primary::DECRYPT},
+    {"TEXT_OCR", demiurge_commands_primary::TEXT_OCR},
+    {"LLM", demiurge_commands_primary::LLM},
+    {"ILC", demiurge_commands_primary::ILC},
+    {"APM", demiurge_commands_primary::APM},
+    {"WAIT", demiurge_commands_primary::REPEAT},
+    {"STOP", demiurge_commands_primary::STOP},
+    {"CLEAR", demiurge_commands_primary::CLEAR},
+    {"EXECUTE", demiurge_commands_primary::EXECUTE},
+    {"C", demiurge_commands_primary::C},
+    {"PLAY_SOUND", demiurge_commands_primary::PLAY_SOUND},
+    {"EDITOR_BG", demiurge_commands_primary::EDITOR_BG},
+    {"EDITOR_FONT", demiurge_commands_primary::EDITOR_FONT},
+    {"EDITOR_TEXT_COLOR", demiurge_commands_primary::EDITOR_TEXT_COLOR},
+    {"EDITOR_CSS", demiurge_commands_primary::EDITOR_CSS},
+    {"DRAW_IMG", demiurge_commands_primary::DRAW_IMG}
+  };
+
+  auto it = commandMap.find(str);
+  return (it != commandMap.end()) ? it->second : demiurge_commands_primary::UNDEFINED;
+}
 
 std::string string_to_decipher;
 
@@ -85,7 +141,7 @@ int main (int argc, char *argv[]) {
         std::string individual_line = seglist[i];
         if (individual_line.empty()) continue;
 
-        
+        std::vector<std::string> space_limiter = splitBySpaces(individual_line);
       }
     } else {
       std::cout << "Unable to open file. It may not exist." << std::endl;
--- src/main.h
+++ src/main.h
@@ -8,6 +8,7 @@
 #include <vector>
 #include <algorithm>
 #include <ranges>
+#include <unordered_map>
 
 #include "make_cpp_easier.h"
 
